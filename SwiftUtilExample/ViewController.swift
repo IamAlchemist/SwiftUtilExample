6a15cbd5375fe27e1fcdcc894d395fbe711035f3
IamAlchemist <Wizard.Lee@qq.com>
2016-08-16 17:56:59 +0800

    init

---
+++ SwiftUtilExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftUtilExample
+//
+//  Created by wizard lee on 8/16/16.
+//  Copyright © 2016 wizard lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
