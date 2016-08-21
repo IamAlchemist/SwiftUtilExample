5c00bbf04c949246b5146dd32798efd39c37cac9
IamAlchemist <Wizard.Lee@qq.com>
2016-08-21 17:34:25 +0800

    finish test dbhelper

---
+++ SwiftUtilExample/TestItem+CoreDataProperties.swift
@@ -0,0 +1,19 @@
+//
+//  TestItem+CoreDataProperties.swift
+//  SwiftUtilExample
+//
+//  Created by wizard lee on 8/20/16.
+//  Copyright © 2016 wizard lee. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension TestItem {
+
+    @NSManaged var name: String?
+
+}
---
+++ SwiftUtilExample/TestItem.swift
@@ -0,0 +1,17 @@
+//
+//  TestItem.swift
+//  SwiftUtilExample
+//
+//  Created by wizard lee on 8/20/16.
+//  Copyright © 2016 wizard lee. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class TestItem: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
